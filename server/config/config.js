87cb9997562f5bd2d9f71ab40f5968a6a9ab90d8
Armando Avelar <pedrooo_1997@hotmail.com>
2019-10-29 12:44:58 -0600

    Rest Server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -4,3 +4,22 @@
 //===================
 
 process.env.PORT = process.env.PORT || 3000;
+
+//======================
+//Entorno
+//=======================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//======================
+//Base de Datos
+//=======================
+
+let urlDB;
+if(process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else{
+    urlDB = 'mongodb+srv://armando:o9a6wuhh8UDVT37O@cluster0-a3owe.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
